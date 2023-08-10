ba3e299d142a4f06edf32d5958438e603878fb4e
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2023-08-10 10:23:27 +0200

    working on framework lesson project implementation
    Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

---
+++ ObjectOrientedProgramming/ClassesAndObjects/IntroducingObjects/src/textures.cpp
@@ -0,0 +1,38 @@
+#include "textures.hpp"
+
+#include <iostream>
+
+const char* getTextureFilename(GameTextureID id) {
+    switch (id) {
+        case GameTextureID::SPACE:
+            return "resources/space.png";
+        case GameTextureID::PLANET:
+            return "resources/planet.png";
+        case GameTextureID::STAR:
+            return "resources/star.png";
+        case GameTextureID::BLACK_HOLE:
+            return "resources/blackhole.png";
+        default:
+            return "";
+    }
+}
+
+bool TextureManager::initialize() {
+    for (size_t i = 0; i < SIZE; ++i) {
+        GameTextureID id = static_cast<GameTextureID>(i);
+        const char* filename = getTextureFilename(id);
+        sf::Texture* texture = &textures[i];
+        if (!texture->loadFromFile(filename)) {
+            std::cerr << "Could not open file " << filename << "\n";
+            return false;
+        }
+        if (id == GameTextureID::SPACE) {
+            texture->setRepeated(true);
+        }
+    }
+    return true;
+}
+
+const sf::Texture* TextureManager::getTexture(GameTextureID id) {
+    return &textures[static_cast<size_t>(id)];
+}
