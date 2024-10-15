6399a533ec3901ddcdd10473e751615bae47fa4c
Dmitrii Kotov <boruno93@gmail.com>
2024-10-15 11:51:38 +0300

    Debug output added to main.cpp

--- ObjectOrientedProgramming/ClassesAndObjects/SceneTransition/src/main.cpp
+++ ObjectOrientedProgramming/ClassesAndObjects/SceneTransition/src/main.cpp
@@ -1,6 +1,8 @@
 #include "engine.hpp"
 
 #include <iostream>
+#include <unistd.h>
+#include <limits.h>
 
 int main() {
     GameEngine* engine = GameEngine::create();
@@ -8,6 +10,14 @@ int main() {
         std::cout << "Game engine is not created!\n";
         return 1;
     }
+    // print current working directory
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        std::cout << "Current working directory: " << cwd << std::endl;
+    } else {
+        std::cerr << "Error getting current working directory" << std::endl;
+    }
+
     GameEngine::create()->run();
     return 0;
 }
